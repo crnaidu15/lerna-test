8a7931fa6c31c96393c4b62367c8f79c52c978c4
Reddaiah Challa <reddaiah.challa@hexagonsi.com>
2019-06-07 14:39:28 +0530

    babel install

--- packages/package-2/src/components/native/Button/Button.js
+++ packages/package-2/src/components/native/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = () => {
     return (
-        <p>Native Button</p>
+        <p>Native Button tag</p>
     )
 }
 
--- packages/package-2/src/components/web/Button/Button.js
+++ packages/package-2/src/components/web/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = () => {
     return (
-        <p>Web Button</p>
+        <p>Web Button tag</p>
     )
 }
 
